00de708c708ea9141bb731fbf5f0ef4fd804a78c
hanna kebede <se.hanna.kebede@gmail.com>
2021-09-04 14:24:05 +0300

    item list

---
+++ lib/itemList.dart
@@ -0,0 +1,201 @@
+import 'package:flutter/material.dart';
+
+class YellowBird extends StatefulWidget {
+  const YellowBird({Key? key}) : super(key: key);
+
+  @override
+  _ListItem createState() => _ListItem();
+}
+
+class _ListItem extends State<YellowBird> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Items List"),
+        ),
+        body: ListView(
+          scrollDirection: Axis.vertical,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+                margin: EdgeInsets.only(left: 70, right: 70, top: 20),
+                child: Column(
+                  children: [
+                    Padding(padding: const EdgeInsets.all(16)),
+                    Text(
+                      "Item List1",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              // color: Colors.grey[100],
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+                margin: EdgeInsets.only(left: 70, right: 70, top: 20),
+                child: Column(
+                  children: [
+                    Padding(padding: const EdgeInsets.all(16)),
+                    Text(
+                      "Item List2",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              // color: Colors.grey[100],
+            ),
+            Container(
+                padding: const EdgeInsets.all(8),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  margin: EdgeInsets.only(left: 70, right: 70, top: 20),
+                  child: Column(
+                    children: [
+                      Padding(padding: const EdgeInsets.all(16)),
+                      Text(
+                        "Item List3",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 25,
+                        ),
+                      ),
+                    ],
+                  ),
+                )),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+                margin: EdgeInsets.only(left: 70, right: 70, top: 20),
+                child: Column(
+                  children: [
+                    Padding(padding: const EdgeInsets.all(16)),
+                    Text(
+                      "Item List4",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              // Container(
+              //   padding: const EdgeInsets.all(8),
+              //   child: Card(
+              //     shape: RoundedRectangleBorder(
+              //         borderRadius: BorderRadius.circular(15)),
+              //     margin: EdgeInsets.only(left: 70, right: 70, top: 20),
+              //     child: Column(
+              //       children: [
+              //         Padding(padding: const EdgeInsets.all(16)),
+              //         Text(
+              //           "Item List5",
+              //           style: TextStyle(
+              //             color: Colors.black,
+              //             fontSize: 25,
+              //           ),
+              //         ),
+              //       ],
+              //     ),
+              //   ),
+
+              //   // color: Colors.grey[100],
+              // ),
+              // Container(
+              //   padding: const EdgeInsets.all(8),
+              //   child: Column(
+              //     children: [
+              //       Image.asset(
+              //         'assets/images/shinkurt.jpg',
+              //         height: 150,
+              //         width: double.infinity,
+              //       ),
+              //       TextButton(
+              //         onPressed: () {},
+              //         child: Text('Karot'),
+              //       ),
+              //     ],
+              //   ),
+              //   // color: Colors.grey[100],
+              // ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+                margin: EdgeInsets.only(left: 70, right: 70, top: 20),
+                child: Column(
+                  children: [
+                    Padding(padding: const EdgeInsets.all(16)),
+                    Text(
+                      "",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+                margin: EdgeInsets.only(left: 70, right: 70, top: 20),
+                child: Column(
+                  children: [
+                    Padding(padding: const EdgeInsets.all(16)),
+                    Text(
+                      "",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: YellowBird(),
+        ),
+      ),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Asbeza menu')),
+      ),
     );
   }
 }
