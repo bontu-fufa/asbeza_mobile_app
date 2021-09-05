3236e1172ba2befffa3a2adf0ecf2eac52a8d770
Edentakele <61546334+Edentakele@users.noreply.github.com>
2021-09-05 11:29:44 -0400

    todo page updated

--- Asbeza_Mobile/lib/presentation/pages/todo.dart
+++ Asbeza_Mobile/lib/presentation/pages/todo.dart
@@ -1,2 +1,86 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+      
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[600],
+      body: Stack(
+        alignment: Alignment.center,
+
+        children: <Widget>[
+          Container(
+            width: 400,
+            height: 600,
+           
+          ),
+
+          Positioned(
+            child: Text("Item List", style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),),
+            top: 40,
+            left: 20,
+          ),
+
+          DraggableScrollableSheet(
+            maxChildSize: 0.85,
+            minChildSize: 0.1,
+            builder: (BuildContext context, ScrollController scrolController){
+              return Stack(
+                overflow: Overflow.visible,
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(topRight: Radius.circular(40), topLeft: Radius.circular(40)),
+                    ),
+                    child: ListView.builder(
+
+                      itemBuilder: (context, index){
+                        return ListTile(
+                          title: Text("Item No $index", style: TextStyle(color: Colors.grey[900], fontWeight: FontWeight.bold),),
+                          subtitle: Text("This is the detail of Item No $index", style: TextStyle(color: Colors.grey[700]),),
+                          trailing: Icon(Icons.check_circle, color: Colors.greenAccent,),
+                          isThreeLine: true,
+                        );
+                      },
+                      controller: scrolController,
+                      itemCount: 20,
+                    ),
+                  ),
+
+                 
+                ],
+              );
+            },
+          )
+
+        ],
+      ),
+    );
+  }
+}
+
