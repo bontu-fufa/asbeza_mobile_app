d5ba596942d014edad8eeb5a661c9568a073a523
hanna kebede <se.hanna.kebede@gmail.com>
2021-09-07 15:22:25 +0300

    add section $ validation

---
+++ Asbeza_Mobile/lib/models/user_model.dart
@@ -0,0 +1,20 @@
+
+class User {
+  final String name;
+  final String password;
+
+  User({required this.name, required this.password});
+
+  factory User.fromJson(Map json) {
+    return User(
+        name: json['user_name'] ?? "", password: json['password'] ?? "");
+  }
+
+  Map<String, String> toMap() {
+    var map = new Map<String, String>();
+    map["user_name"] = name;
+    map["password"] = password;
+
+    return map;
+  }
+}
--- Asbeza_Mobile/lib/presentation/pages/login.dart
+++ Asbeza_Mobile/lib/presentation/pages/login.dart
@@ -1,4 +1,8 @@
+import 'package:asbeza_mobile_app/models/user_model.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -7,8 +11,45 @@ class LoginScreen extends StatefulWidget {
   _LoginScreenState createState() => _LoginScreenState();
 }
 
+// function for checking user name and password from api
+Future getUser(Map body) async {
+  return http
+      .post(
+          Uri.parse("http://10.0.2.2:5000/asbeza/api/v1/users/login"),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: jsonEncode(body))
+      .then((http.Response response) {
+          final int statusCode = response.statusCode;
+
+          if (statusCode < 200 || statusCode > 400 || json == null) {
+            throw new Exception("Error while fetching data");
+          }
+          
+          return json.decode(response.body);
+  });
+}
+
+
+Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+Future addToSession(Map value) async{
+  final SharedPreferences prefs = await _prefs;
+  prefs.setInt("user_id", value['currentUserId']);
+  prefs.setString("user_name", value['currentUserName']);
+  prefs.setString("email", value['currentUserEmail']);
+  prefs.setString("user_type", value['currentUserType']);
+
+  return prefs;
+}
+
 class _LoginScreenState extends State<LoginScreen> {
   bool hidePassword = true;
+  
+  final formKey = GlobalKey<FormState>();
+  final nameController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,10 +59,12 @@ class _LoginScreenState extends State<LoginScreen> {
       body: Container(
         padding: EdgeInsets.fromLTRB(10, 15, 10, 0),
         child: Form(
+          key: formKey,
           child: Container(
             child: Column(
               children: [
                 TextFormField(
+                  controller: nameController,
                   decoration: InputDecoration(
                     hintText: "Username",
                     border: OutlineInputBorder(
@@ -31,11 +74,15 @@ class _LoginScreenState extends State<LoginScreen> {
                       borderRadius: BorderRadius.circular(100.0),
                     ),
                   ),
+                  validator: (String? name) {
+                    if (name == "") return "Enter username";
+                  },
                 ),
                 SizedBox(
                   height: 10,
                 ),
                 TextFormField(
+                  controller: passwordController,
                   obscureText: hidePassword ? true : false,
                   enableSuggestions: false,
                   autocorrect: false,
@@ -61,25 +108,42 @@ class _LoginScreenState extends State<LoginScreen> {
                       borderRadius: BorderRadius.circular(100.0),
                     ),
                   ),
+                  validator: (String? password) {
+                    if (password == "") return "Enter password";
+                  },
                 ),
                 SizedBox(
                   height: 30,
                 ),
                 GestureDetector(
                   onTap: () {
-                    // run validations.
-                    // form access.
-                    // final valid = formKey.currentState!.validate();
-                    // if (!valid) {
-                    //   // do something here.
-                    //   print("something failed");
-                    //   return;
-                    // }
-
-                    // final email = emailTextController.text;
-                    // final pass = passwordTextController.text;
-                    // print("all went well");
-                    // print("email:: $email  pass:: $pass");
+                    // run validations
+                    final valid = formKey.currentState!.validate();
+                      if (!valid) {
+                        // do something here.
+                        print("something failed");
+                        return;
+                      }
+                    // Get input and validate values from api
+                    User user = User(
+                        name: nameController.text,
+                        password: passwordController.text);
+                    getUser(user.toMap()).then((value) {
+                      if (value['message'] == 'logged in') {
+                        // TODO: navigate to home page
+                        addToSession(value)
+                        .then((value) {
+                          print(value.getInt('user_id'));
+                          print(value.getString('user_name'));
+                          print(value.getString('email'));
+                          print(value.getString('user_name'));
+                        });
+                        print("logged in");
+                      } else {
+                        // TODO: display error message
+                        print("not logged in");
+                      }
+                    });
                   },
                   child: Container(
                     height: 60.0,
