49afa7cdb426cb20e56c9d0bdebd373e16fe5fc4
hacker <hacker.se.eden.takele@gmail.com>
2021-09-11 04:49:17 -0400

    list_screen_items_testing

--- Asbeza_Mobile/test/auth_test/auth_bloc_test.dart
+++ Asbeza_Mobile/test/auth_test/auth_bloc_test.dart
@@ -1,16 +1,44 @@
-import 'package:asbeza_mobile_app/auth/screens/profile.dart';
+import 'package:asbeza_mobile_app/item/screens/itemList.dart';
+import 'package:asbeza_mobile_app/item/screens/post_item_price.dart';
+import 'package:asbeza_mobile_app/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 
+
+
 void main() {
-   testWidgets('check for the text in the appbar',
+   testWidgets('check for scaffold in itemlist',
        (WidgetTester tester) async {
      await tester
-         .pumpWidget(MaterialApp(home: ProfileApp(),));
+         .pumpWidget(ItemList(),);
 
-     final messageFinder = find.byType(AppBar);
+     final messageFinder = find.byType(Scaffold);
 
      expect(messageFinder, findsOneWidget);
+   });
+   testWidgets('check for appbar in itemlist', (WidgetTester tester) async {
+     await tester
+         .pumpWidget(ItemList());
+
+      final messagee = find.byType(AppBar);
+
+      expect(messagee, findsOneWidget);
+   });
+   testWidgets("ListView", (WidgetTester tester) async{
+     await tester.pumpWidget(ItemList());
+     final messages = find.byType(ListView);
+     expect(messages, findsOneWidget);
+   });
+   testWidgets('GestureDetector', (WidgetTester tester) async{
+     await tester.pumpWidget(MaterialApp(home: PostItemPriceScreen()));
+
+     final search = find.byType(GestureDetector);
+     expect(search, findsOneWidget);
+   });
+   testWidgets('splash screen', (WidgetTester tester) async {
+     await tester.pumpWidget(ItemList());
+     final splash = find.byType(Container);
+     expect(splash, findsNWidgets(12));
    });
 }
